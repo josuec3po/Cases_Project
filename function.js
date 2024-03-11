b853e589ab1ba3688a2ee00639c3df230c388950
Josh1 <josuefbraga@outlook.com>
2024-03-11 02:37:02 +0000

    function changes

--- function.js
+++ function.js
@@ -1,33 +1,32 @@
-function convertUpperCase() {
-    let inputText = document.getElementById("txtvalue")
-    inputText.value = inputText.value.toUpperCase()
-}
 
-function convertLowerCase() {
-    let inputText = document.getElementById("txtvalue")
-    inputText.value = inputText.value.toLowerCase()
-}
+function caseConversion(tipo) {
 
-function convertAlternatedCase() {
-    let inputText = document.getElementById("txtvalue")
-    let inputString = inputText.value
-    let result = ""
-    for (let i = 0; i < inputString.length; i++) {
-        if (i % 2 === 0) {
-            result += inputString[i].toUpperCase()
-        } else {
-            result += inputString[i].toLowerCase()
-        }
+    let inputString = document.getElementById('txtvalue')
+
+    switch (tipo) {
+        // Enclose conversion types 'upper' and 'lower' in single quotes to treat them as strings
+        case 'upper':
+            inputString.value = inputString.value.toUpperCase()
+            break;
+        case 'lower':
+            inputString.value = inputString.value.toLowerCase()
+            break;
+        case 'alternated':
+            let inputStr = inputString.value
+            let result = ''
+            for (let i = 0; i < inputStr.length; i++) { // Attention to VAR reference
+                if (i % 2 === 0) {
+                    result += inputStr[i].toUpperCase()
+                } else {
+                    result += inputStr[i].toLowerCase()
+                }
+            }
+            inputString.value = result
+            break;
+        case 'reversed':
+            let x = inputString.value
+            x = x.split('').reverse().join('') // Correct the variable used for string manipulation from 'inputString' to 'x'
+            inputString.value = x
+            break;
     }
-    inputText.value = result
-}
-
-function convertReverseCase() {
-    let inputText = document.getElementById("txtvalue")
-    let inputStr = inputText.value
-    let result = ""
-    for (let i = inputStr.length - 1; i >= 0; i--) {
-        result += inputStr[i]
-    }
-    inputText.value = result
 }
